2296c81473c2c8d5db4736d21edd022f8522b248
渡辺　将光（SBT　技術統括　PE　エンジニアリング統括部） <masamiwatanabe@tech.softbank.co.jp>
2018-03-27 14:38:07 +0900

    loginpage

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ header("Content-Type: text/html; charset=Shift_JIS");
 <html lang = "ja">
 
 <head>
-<meta charset = "UFT-8">
-<title>アカウントの情報を取得する</title>
+<title>HTTPとHTTPS通信内容の違い</title>
 <style type="text/css">
 
 dl.profile dt{
@@ -28,13 +27,14 @@ dl.profile dd{
 </head>
 
 <body>
-<h3>アカウントの情報を取得します。</h3>
-<h3>SQLインジェクションのサンプルです。</h3></br>
+<h3>HTTPとHTTPS通信内容の違い</h3>
+<h3>このサイトにhttp://とhttps://のそれぞれでアクセスし</h3></br>
+<h3>ログインをして、WireSharkで通信内容を確認してみましょう。</h3></br>
 <h4>ユーザ名とパスワードを入力してください</h4>
-<form action = "index.php" method = "post">
+<form action = "loginsuccess.php" method = "post">
 <dl class="profile">
 <dt>ユーザ名：</dt>
-<dd><input type = “text” name =“comment/“></dd>
+<dd><input type = “text” name =“userid/“></dd>
 
 <script>
 function checkPassword(e) {
@@ -48,17 +48,10 @@ function checkPassword(e) {
 </script>
 
 <dt>パスワード：</dt>
-<dd><input type="password" id="password" /> <input type="checkbox" onclick="checkPassword(this)" /></dd>
+<dd><input type="password" id="password" name ="pass/" /> <input type="checkbox" onclick="checkPassword(this)" /></dd>
 </dl>
 <button type="submit" name="action" value="send">ログイン</button>
 </form>
-
-<?php
-if(isset($_POST[‘comment’])){
-$comment = $_POST[‘comment’];
-echo $comment;
-}
-?>
 
 </body>
 </html>
---
+++ loginsuccess.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-Type: text/html; charset=Shift_JIS");
+?>
+
+<!DOCTYPE html>
+<html lang = "ja">
+
+<head>
+<title>ログイン後ページ</title>
+</head>
+
+<body>
+<h1>ユーザーIDとパスワードの送信</h1>
+<h2>ログイン後ページ</h2>
+<p>ユーザIDは：
+<?php
+ echo htmlspecialchars($_POST["userid"], ENT_QUOTES, "UTF-8"); ?>
+</p><br>
+<p>パスワードは：
+<?php
+ echo htmlspecialchars($_POST["pass"], ENT_QUOTES, "UTF-8"); ?>
+</p><br>
+
+<input value="もどる" onclick="history.back();" type="button">
+
+</body>
+</html>
