a9c862f035f9706ecdfb5026567d73e39d7bf0f8
KevinMasa <necolamasa1@gmail.com>
2018-04-21 01:03:32 +0900

    sql

--- sqlinjection/loginsuccess.php
+++ sqlinjection/loginsuccess.php
@@ -1,4 +1,7 @@
 
+<?php
+header("Content-Type: text/html; charset=Shift_JIS");
+?>
 <!DOCTYPE html>
 <html lang = "ja">
 
@@ -11,6 +14,26 @@
 <h1>ユーザーIDとパスワードの送信</h1>
 <h2>ログイン後ページ</h2>
 <p>
+<?php 
+$link = mysql_connect('fy18myserver.mysql.database.azure.com', 'fy18admin', 'Fy18_admin');
+if (!$link) {
+    die('接続失敗です。'.mysql_error());
+}
+
+$db_selected = mysql_select_db('maindb', $link);
+if (!$db_selected){
+    die('データベース選択失敗です。'.mysql_error());
+}
+
+$result = mysql_query('SELECT * from maintable');
+if (!$result) {
+    die('クエリーが失敗しました。'.mysql_error());
+}
+
+mysql_close($link); ?>
+
+<?php exho $result ?>
+
 <?php
  echo htmlspecialchars($_POST["comment"], ENT_QUOTES, "UTF-8"); ?>
 </p>様ようこそ<br>
