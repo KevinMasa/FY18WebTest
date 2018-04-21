bea202ec90ce249310f80e082be20b2e8ee0e770
渡辺　将光（SBT　技術　SE　セキュリティサービス部） <masamiwatanabe@tech.softbank.co.jp>
2018-04-21 14:57:53 +0900

    sql

--- sqlinjection/loginsuccess.php
+++ sqlinjection/loginsuccess.php
@@ -30,7 +30,7 @@ echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
- echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+ echo ($row['id'] . " " . $row['password'] . PHP_EOL);
 }
 sqlsrv_free_stmt($getResults);
 
