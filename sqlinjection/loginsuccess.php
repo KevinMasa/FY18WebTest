b796c3cc9249b9ef4d2fafec37bdc216a2e40b08
渡辺　将光（SBT　技術　SE　セキュリティサービス部） <masamiwatanabe@tech.softbank.co.jp>
2018-04-21 13:41:28 +0900

    sql

---
+++ sqlinjection/loginsuccess.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang = "ja">
+
+<head>
+<meta charset = "UFT-8">
+<title>ログイン後</title>
+</head>
+
+<body>
+<h1>ユーザーIDとパスワードの送信</h1>
+<h2>ログイン後ページ</h2>
+<p>
+<?php 
+$host = 'fy18myserver.mysql.database.azure.com';
+$username = 'fy18admin@fy18myserver';
+$password = 'Fy18_admin';
+$db_name = 'maindb';
+
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT id,password WHERE id="fy18admin" FROM maintable');
+while ($row = mysqli_fetch_assoc($res)) {
+  echo "id:", $row['id'];
+  echo "\t";
+  echo "password:",$row['password'];
+}
+
+mysqli_close($conn);
+?>
+
+</p>
+</body>
+</html>
