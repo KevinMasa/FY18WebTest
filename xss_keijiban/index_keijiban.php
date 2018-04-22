8fe6c5e1058747f868af1734b6b4081d27e5a3fa
渡辺　将光（SBT　技術　SE　セキュリティサービス部） <masamiwatanabe@tech.softbank.co.jp>
2018-04-22 15:56:47 +0900

    sql

--- xss_keijiban/index_keijiban.php
+++ xss_keijiban/index_keijiban.php
@@ -7,49 +7,36 @@ header("Content-Type: text/html; charset=UTF-8");
 
 <head>
 <meta charset = "UTF-8">
-<title>掲示板</title>
-<style type="text/css">
-body{
-    text-align:center;
-}
-.label{
-    vertical-align: top;
-    margin-right: 5px;
-}
-.toukou{
-    border-top: 1px solid;
-    margin: 40px auto;
-    max-width: 975px;
-}
-
-</style>
+<title>SQLインジェクション(脆弱)</title>
 </head>
 
 <body>
+<h2>アカウント情報の取得</h2>
 
-<h3>クロスサイトスクリプティングのサンプルページです。</h3>
+<?php 
+$inputid = $_POST["id"];
+$tsql= "SELECT *FROM maintb WHERE name='".$inputid."'";
+echo "渡されたSQL文　「".htmlspecialchars($tsql,ENT_QUOTES)."」";
 
-<h1>掲示板</h1>
-<section>
-    <h2>新規投稿</h2>
-        <form action="" method="post">
-               <div class="name"><span class="label">お名前(32文字以内):</span><input type="text" name="name" value=""></div></br>
-        <div class="honbun"><span class="label">本文(200文字以内):</span><textarea name="comment" cols="30" rows="3" maxlength="80" wrap="hard" placeholder="200字以内で入力してください。"></textarea></div>
-        <input type="submit" value="投稿">
-    </form>
-</section>
-<section class="toukou">
-    <h2 >投稿一覧</h2>
-<!--
-<?php
-     if(isset($_POST['comment'])){
-         echo "<p>投稿はまだありません</p>";
-     }else{
-$personal_name = "'".$_POST['name']."'";
-$personal_comment = "'".$_POST['comment']."'";
-$nowdate = date(Y-m-d);
-$nowtime = date(H:i:s);
-$now = "'".$nowdate." ".$nowtime."'";
+?>
+<br>
+↑ nameの後は「'」シングルクォートです。
+<br>
+SELECT * FROM maintb WHERE name ='○○' (maintbというテーブルからnameが○○の情報を抽出します。)
+<br>
+WHERE句が全てを意味するように○○に入力する。
+<br>
+<br>
+
+
+<table border="1">
+<tr>
+<th>id</th>
+<th>name</th>
+<th>password</th>
+</tr>
+<?php 
+$inputid = $_POST["id"];
 
 $serverName = "fy18test.database.windows.net";
 $connectionOptions = array(
@@ -58,25 +45,65 @@ $connectionOptions = array(
     "PWD" => "Fy18_test"
 );
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql= "INSERT INTO keijiban(name,contents,time) VALUES($personal_name,$personal_comment,$now)";
+$tsql= "SELECT *FROM maintb WHERE name='".$inputid."'";
+
 
 $getResults= sqlsrv_query($conn, $tsql);
 
+
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo "<tr>";
+ echo "<td>".$row['id']."</td>";
+ echo "<td>".$row['name']."</td>";
+ echo "<td>".$row['password']."</td>";
+// echo ("id:".$row['name'] . " " . "password:".$row['password'] . PHP_EOL);
+ echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
 
+//%' OR 1 = 1; -- %
 
-$data = "<hr>\r\n";
-$data = $data."<p>投稿者:".$personal_name."</p>\r\n";
-$data = $data."<p>内容:</p>\r\n";
-$data = $data."<p>".$contents."</p>\r\n";
-
-print('<p>投稿者:'.$personal_name.'</p>');
-print('<p>内容:</p>');
-print('<p>'.$contents.'</p>');
 ?>
+
+</table>
+
+<!--phpコード↓
+
+$inputid = $_POST["id"];
+
+$serverName = "fy18test.database.windows.net";
+$connectionOptions = array(
+    "Database" => "fy18test",
+    "Uid" => "fy18test",
+    "PWD" => "Fy18_test"
+);
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$tsql= "SELECT *FROM maintb WHERE name='".$inputid."'";
+
+
+$getResults= sqlsrv_query($conn, $tsql);
+
+
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo "<tr>";
+ echo "<td>".$row['id']."</td>";
+ echo "<td>".$row['name']."</td>";
+ echo "<td>".$row['password']."</td>";
+// echo ("id:".$row['name'] . " " . "password:".$row['password'] . PHP_EOL);
+ echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
+
+//%' OR 1 = 1; -- %
+
+
 -->
-</section>
+<br>
+<button type="button" onclick="history.back()">戻る</button>
+
 </body>
 </html>
-
